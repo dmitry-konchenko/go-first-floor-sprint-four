a7bcd70c769490defc91536d9d4aeba758da74a9
Dmitry Konshenko <scp547228@gmail.com>
2026-05-30 00:55:22 +0300

    fixed main go problem

--- cmd/tracker/main.go
+++ cmd/tracker/main.go
@@ -4,8 +4,8 @@ import (
 	"fmt"
 	"log"
 
-	"github.com/dmitry-konchenko/go-first-floor-sprint-four/tracker/internal/daysteps"
-	"github.com/dmitry-konchenko/go-first-floor-sprint-four/tracker/internal/spentcalories"
+	"github.com/dmitry-konchenko/go-first-floor-sprint-four/internal/daysteps"
+	"github.com/dmitry-konchenko/go-first-floor-sprint-four/internal/spentcalories"
 )
 
 func main() {
